594997391ee21e6e943d297578b48cc324b6b909
Daniel Espendiller <daniel@espendiller.net>
2017-05-04 11:15:23 +0200

    use RunConfigurationProducer instance factory for PhpUnit runtime producer

--- src/de/espend/idea/php/phpunit/utils/PhpUnitPluginUtil.java
+++ src/de/espend/idea/php/phpunit/utils/PhpUnitPluginUtil.java
@@ -3,10 +3,10 @@ package de.espend.idea.php.phpunit.utils;
 import com.intellij.execution.ProgramRunnerUtil;
 import com.intellij.execution.actions.ConfigurationContext;
 import com.intellij.execution.actions.ConfigurationFromContext;
+import com.intellij.execution.actions.RunConfigurationProducer;
 import com.intellij.execution.executors.DefaultDebugExecutor;
 import com.intellij.psi.PsiElement;
 import com.jetbrains.php.phpunit.PhpUnitRuntimeConfigurationProducer;
-import com.jetbrains.php.run.deploymentAware.phpunit.PhpUnitRemoteRuntimeConfigurationProducer;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -19,7 +19,7 @@ public class PhpUnitPluginUtil {
      * @param psiElement Elements are PhpClass or Method possible context
      */
     public static void executeDebugRunner(@NotNull PsiElement psiElement) {
-        ConfigurationFromContext context = PhpUnitRemoteRuntimeConfigurationProducer.getInstance(PhpUnitRuntimeConfigurationProducer.class)
+        ConfigurationFromContext context = RunConfigurationProducer.getInstance(PhpUnitRuntimeConfigurationProducer.class)
             .createConfigurationFromContext(new ConfigurationContext(psiElement));
 
         if(context != null) {
