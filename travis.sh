3f27aa55ef94c2a88a1cb2c635fd0ee6399f66c6
Daniel Espendiller <espendiller@gmx.de>
2017-10-27 19:31:37 +0200

    update travis build

--- travis.sh
+++ travis.sh
@@ -3,6 +3,10 @@
 ideaVersion="2017.1"
 if [ "$PHPSTORM_ENV" == "2017.1" ]; then
     ideaVersion="2017.1.4"
+elif [ "$PHPSTORM_ENV" == "2017.2" ]; then
+    ideaVersion="2017.2.4"
+elif [ "$PHPSTORM_ENV" == "2017.2.4" ]; then
+    ideaVersion="2017.2.4"
 elif [ "$PHPSTORM_ENV" == "eap" ]; then
     ideaVersion="163.5644.15"
 fi
